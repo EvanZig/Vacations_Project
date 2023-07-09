18f23601fd0892617a196a953a61a15ca808d28d
GitSkrud <euaggeloszigas@gmail.com>
2023-07-10 01:54:08 +0300

    addded ui for VacationsStatus component

--- client/src/employee/VacationsStatus.js
+++ client/src/employee/VacationsStatus.js
@@ -1,5 +1,71 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Table, Button } from 'antd'
+
+const columns = [
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email',
+    },
+    {
+        title: '',
+        dataIndex: 'dev',
+        key: 'dev',
+    },
+]
+
+const dataSource = [
+    {
+        key: '1',
+        name: 'Mike',
+        email: 32,
+        dev: 'delete',
+    },
+    {
+        key: '2',
+        name: 'John',
+        email: 42,
+        dev: (
+            <>
+                <a>edit</a> <a>delete</a>
+            </>
+        ),
+    },
+]
 
 export default function VacationsStatus() {
-    return <div></div>
+    const [vacationRequests, setVacationRequests] = useState([])
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:5000/show')
+            .then((response) => {
+                console.log(response.data)
+                const parsedData = JSON.parse(response.data)
+                setVacationRequests(parsedData)
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+    }, [])
+
+    return (
+        <div>
+            <h2 style={{ display: 'flex', justifyContent: 'center' }}>
+                List of users
+            </h2>
+            <Button>Create User</Button>
+            <Table
+                dataSource={dataSource}
+                columns={columns}
+                pagination={false}
+            />
+        </div>
+    )
 }
