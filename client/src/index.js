6f5f15456df6f62e891fa30ce1c31b5e0e4d4d5e
GitSkrud <euaggeloszigas@gmail.com>
2023-07-10 02:25:31 +0300

    added UserCreation component UI

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,14 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import VacationsStatus from './employee/VacationsStatus'
 import UsersList from './manager/UsersList'
+import UserCreation from './manager/UserCreation'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <React.StrictMode>
         {/* <App /> */}
-        <UsersList />
+        {/* <UsersList /> */}
+        <UserCreation />
     </React.StrictMode>
 )
 
--- client/src/manager/UserCreation.js
+++ client/src/manager/UserCreation.js
@@ -1,5 +1,74 @@
 import React from 'react'
+import { Button, Input, Form } from 'antd'
+import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons'
 
 export default function UserCreation() {
-    return <div></div>
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <div style={{ width: '600px' }}>
+                <h2 style={{ textAlign: 'center' }}>User properties</h2>
+                <Form
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                    }}
+                    labelCol={{ flex: '80px' }}
+                    labelAlign="left"
+                    labelWrap
+                >
+                    <Form.Item
+                        label="Name"
+                        name="name"
+                        style={{ marginTop: '20px' }}
+                    >
+                        <Input
+                            placeholder="name"
+                            allowClear
+                            className="formInput"
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        label="Email"
+                        name="email"
+                        style={{ marginTop: '20px' }}
+                    >
+                        <Input
+                            placeholder="email"
+                            allowClear
+                            className="formInput"
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        className="formItem"
+                        style={{ marginTop: '20px' }}
+                    >
+                        <Input.Password
+                            placeholder="Password"
+                            iconRender={(visible) =>
+                                visible ? (
+                                    <EyeTwoTone />
+                                ) : (
+                                    <EyeInvisibleOutlined />
+                                )
+                            }
+                            className="formInput"
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        style={{ display: 'flex', justifyContent: 'center' }}
+                    >
+                        <Button
+                            type="primary"
+                            htmlType="submit"
+                            style={{ width: '100px' }}
+                        >
+                            Save
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
+        </div>
+    )
 }
