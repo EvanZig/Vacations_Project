881a5c448463d7823c8b67618c79f697fb174f11
GitSkrud <euaggeloszigas@gmail.com>
2023-07-10 01:56:41 +0300

    ui code transfered to its actual component

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,13 @@ import './styles/index.scss'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import VacationsStatus from './employee/VacationsStatus'
+import UsersList from './manager/UsersList'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <React.StrictMode>
         {/* <App /> */}
-        <VacationsStatus />
+        <UsersList />
     </React.StrictMode>
 )
 
