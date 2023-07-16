3ca38020a74281be22f1a072bcd932efdd7dd708
GitSkrud <euaggeloszigas@gmail.com>
2023-07-16 18:48:01 +0300

    finished allVacations api and added authentication

--- client/src/employee/VacationsStatus.js
+++ client/src/employee/VacationsStatus.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import axios from 'axios'
 import { Table, Button } from 'antd'
+import { AuthContext } from '../Routes/context'
 
 const columns = [
     {
@@ -30,35 +31,45 @@ const columns = [
     },
 ]
 
-const dataSource = [
-    {
-        key: '1',
-        dateSubmitted: 'Mike',
-        totalDays: 32,
-        status: 'delete',
-    },
-    {
-        key: '2',
-        dateSubmitted: 'John',
-        totalDays: 42,
-        status: (
-            <>
-                <a>edit</a> <a>delete</a>
-            </>
-        ),
-    },
-]
-
 export default function VacationsStatus() {
     const [vacationRequests, setVacationRequests] = useState([])
+    const authContext = useContext(AuthContext)
+
+    const calculateDays = (startDate, endDate) => {
+        const dateFrom =
+            typeof startDate === 'string' ? new Date(startDate) : startDate
+        const dateTo = typeof endDate === 'string' ? new Date(endDate) : endDate
+
+        // Calculate the difference in milliseconds
+        const differenceMs = dateTo - dateFrom
+
+        // Convert milliseconds to days
+        const oneDayInMs = 1000 * 60 * 60 * 24
+        const numberOfDays = Math.round(differenceMs / oneDayInMs)
+        return numberOfDays
+    }
 
     useEffect(() => {
         axios
-            .get('http://localhost:5000/show')
+            .get('http://localhost:8888/allVacations', {
+                headers: {
+                    Authorization: `Bearer ${authContext.token}`,
+                },
+            })
             .then((response) => {
                 console.log(response.data)
-                const parsedData = JSON.parse(response.data)
-                setVacationRequests(parsedData)
+                const formattedData = response.data.map((item, index) => ({
+                    key: index + 1,
+                    dateSubmitted: item.submitionDate,
+                    dateRequested: item.vacation_date_from,
+                    totalDays: calculateDays(
+                        item.vacation_date_from,
+                        item.vacation_date_to
+                    ),
+                    reason: item.reason,
+                    status: item.status,
+                }))
+                setVacationRequests(formattedData)
             })
             .catch((error) => {
                 console.error(error)
@@ -72,7 +83,7 @@ export default function VacationsStatus() {
             </h2>
             <Button>Request vacation</Button>
             <Table
-                dataSource={dataSource}
+                dataSource={vacationRequests}
                 columns={columns}
                 pagination={false}
             />
