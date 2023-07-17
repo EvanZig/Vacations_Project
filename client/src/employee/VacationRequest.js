81fde14d220ee03d2266eeb0bc49098ba9bb61ff
GitSkrud <euaggeloszigas@gmail.com>
2023-07-17 15:51:28 +0300

    added reset form on request

--- client/src/employee/VacationRequest.js
+++ client/src/employee/VacationRequest.js
@@ -1,12 +1,14 @@
 import React, { useContext, useState } from 'react'
-import { DatePicker, Input, Button } from 'antd'
+import { DatePicker, Input, Button, Form } from 'antd'
 import axios from 'axios'
 import { AuthContext } from '../Routes/context'
 import Return from '../Return'
+import { useForm } from 'antd/es/form/Form'
 
 const { TextArea } = Input
 
 export default function VacationRequest() {
+    const [form] = useForm()
     const [dateStrings, setDateStrings] = useState({
         dateFrom: '',
         dateTo: '',
@@ -41,11 +43,15 @@ export default function VacationRequest() {
             })
             .then((response) => {
                 console.log(response.data)
+                form.resetFields()
                 alert('Vacation Requested Submitted')
             })
             .catch((error) => {
                 console.log(error)
+                form.resetFields()
+                alert('Vacation Request failed')
             })
+        form.resetFields()
     }
     return (
         <div
@@ -57,42 +63,59 @@ export default function VacationRequest() {
             }}
         >
             <Return />
-            <div style={{ width: '400px' }}>
-                <h2 style={{ textAlign: 'center' }}>Vacation Request</h2>
-                <div style={{ marginTop: '20px' }}>
-                    <label>Date from: </label>
-                    <DatePicker
-                        onChange={(date, dateString) =>
-                            onDateChange('dateFrom', date, dateString)
-                        }
-                    />
+            <Form form={form}>
+                <div style={{ width: '400px' }}>
+                    <h2 style={{ textAlign: 'center' }}>Vacation Request</h2>
+                    <div style={{ marginTop: '20px' }}>
+                        <Form.Item
+                            label="Date from:"
+                            name="dateFrom"
+                            style={{ marginTop: '20px' }}
+                        >
+                            <DatePicker
+                                onChange={(date, dateString) =>
+                                    onDateChange('dateFrom', date, dateString)
+                                }
+                                name="dateFrom"
+                            />
+                        </Form.Item>
+                    </div>
+                    <div style={{ marginTop: '20px' }}>
+                        <Form.Item
+                            label="Date from:"
+                            name="dateTo"
+                            style={{ marginTop: '20px' }}
+                        >
+                            <DatePicker
+                                onChange={(date, dateString) =>
+                                    onDateChange('dateTo', date, dateString)
+                                }
+                                name="dateTo"
+                            />
+                        </Form.Item>
+                    </div>
+                    <div style={{ marginTop: '20px' }}>
+                        <Form.Item label="Date from:" name="reason">
+                            <TextArea
+                                showCount
+                                maxLength={100}
+                                className="textarea-reason"
+                                onChange={(e) =>
+                                    onChange('reason', e.target.value)
+                                }
+                                placeholder="Add reason"
+                                name="reason"
+                            />
+                        </Form.Item>
+                    </div>
+                    <Button
+                        style={{ marginTop: '60px', marginLeft: '150px' }}
+                        onClick={request}
+                    >
+                        Request
+                    </Button>
                 </div>
-                <div style={{ marginTop: '20px' }}>
-                    <label>Date to: </label>
-                    <DatePicker
-                        onChange={(date, dateString) =>
-                            onDateChange('dateTo', date, dateString)
-                        }
-                        style={{ marginLeft: '20px' }}
-                    />
-                </div>
-                <div style={{ marginTop: '20px' }}>
-                    <label>Reason: </label>
-                    <TextArea
-                        showCount
-                        maxLength={100}
-                        className="textarea-reason"
-                        onChange={(e) => onChange('reason', e.target.value)}
-                        placeholder="Add reason"
-                    />
-                </div>
-                <Button
-                    style={{ marginTop: '60px', marginLeft: '150px' }}
-                    onClick={request}
-                >
-                    Request
-                </Button>
-            </div>
+            </Form>
         </div>
     )
 }
